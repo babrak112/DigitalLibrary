5c1535b11f57a7ffe9f23dbcd512c4615530f850
danielntemo <ntemo.daniel@seznam.cz>
2025-03-15 23:20:20 +0100

    Create app Library + models + templates + views + url

---
+++ library/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render
+from django.views.generic import DetailView, ListView
+
+from library.models import Book, Author
+
+
+def home(request):
+    return render(request, 'home.html')
+
+
+class BookDetailView(DetailView):
+    model = Book
+    context_object_name = 'book'
+    template_name = 'book.html'
+
+class BookListView(ListView):
+    model = Book
+    context_object_name = 'books'
+    template_name = 'books.html'
+
+
+class AuthorDetailView(DetailView):
+    model = Author
+    context_object_name = 'author'
+    template_name = 'author.html'
+
+class AuthorListView(ListView):
+    model = Author
+    context_object_name = 'authors'
+    template_name = 'authors.html'
+
+
+
+
+
